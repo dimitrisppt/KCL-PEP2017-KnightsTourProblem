ff29d99464659133793ba74b5b095c8e3ab451c0
AndrewColes <andrew.i.coles@googlemail.com>
2017-11-28 23:45:02 +0000

    Add files via upload

---
+++ collatz.h
@@ -0,0 +1,27 @@
+#ifndef COLLATZ_H
+#define COLLATZ_H
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <cstdint>
+#include "counting_iterator.h"
+#include "transformreduce.h"
+
+// Do not add any extra #includes without asking on the KEATS discussion forum
+
+uint64_t collatz(uint64_t n) {
+    // TODO: your code goes here
+}
+
+std::pair<uint64_t, uint64_t> collatz_max(uint64_t bound) {
+  
+    // TODO: your code goes here
+
+
+}
+
+// Do not edit anything below this line
+
+#endif
---
+++ knights.h
@@ -0,0 +1,32 @@
+#ifndef KNIGHTS_H
+#define KNIGHTS_H
+
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <cstdint>
+
+// Do not add any extra #includes without asking on the KEATS discussion forum
+
+using std::pair;
+using std::make_pair;
+using std::vector;
+
+typedef vector<pair<int,int> > Path;
+
+/** Helper function: adds two pairs of ints */
+pair<int,int> operator+(const pair<int,int> & a, const pair<int,int> & b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+// TODO - your code goes here
+
+
+
+
+
+// Do not edit below this line
+
+#endif
